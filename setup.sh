ff79902690a9206d54e0aaf34bf268f2cb3c4a6f
Badgy <jonaskhansen@gmail.com>
2020-04-30 15:16:05 +0200

    quote paths

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo ln -s $HOME/dotjellies/nginx/conf.d/jellyfin.conf /etc/nginx/conf.d/jellyfin.conf
-sudo ln -s $HOME/dotjellies/systemd/system/certbot.service /etc/systemd/system/certbot.service
-sudo ln -s $HOME/dotjellies/systemd/system/certbot.timer /etc/systemd/system/certbot.timer
+sudo ln -s "$HOME/dotjellies/nginx/conf.d/jellyfin.conf" "/etc/nginx/conf.d/jellyfin.conf"
+sudo ln -s "$HOME/dotjellies/systemd/system/certbot.service" "/etc/systemd/system/certbot.service"
+sudo ln -s "$HOME/dotjellies/systemd/system/certbot.timer" "/etc/systemd/system/certbot.timer"
