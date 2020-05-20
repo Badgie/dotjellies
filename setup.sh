fd8365872b3b5f8bf51b9c1737e0b82a91cc8f47
Badgy <jonaskhansen@gmail.com>
2020-05-20 18:37:26 +0200

    update setup script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 sudo ln -s "$HOME/dotjellies/nginx/conf.d/jellyfin.conf" "/etc/nginx/conf.d/jellyfin.conf"
+sudo ln -s "$HOME/dotjellies/nginx/conf.d/jstat.conf" "/etc/nginx/conf.d/jstat.conf"
 sudo ln -s "$HOME/dotjellies/systemd/system/certbot.service" "/etc/systemd/system/certbot.service"
 sudo ln -s "$HOME/dotjellies/systemd/system/certbot.timer" "/etc/systemd/system/certbot.timer"
+sudo ln -s "$HOME/dotjellies/systemd/system/jstat.service" "/etc/systemd/system/jstat.service"
+
