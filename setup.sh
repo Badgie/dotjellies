213e19119933a661b8871942b5f4161fe4e1cc8c
Badgy <jonaskhansen@gmail.com>
2020-05-27 18:55:37 +0200

    add server side graph stuff

--- setup.sh
+++ setup.sh
@@ -7,7 +7,10 @@ sudo ln -s "$HOME/dotjellies/systemd/system/certbot.timer" "/etc/systemd/system/
 sudo ln -s "$HOME/dotjellies/systemd/system/jstat.service" "/etc/systemd/system/jstat.service"
 sudo ln -s "$HOME/dotjellies/systemd/system/rpistats.service" "/etc/systemd/system/rpistats.service"
 sudo ln -s "$HOME/dotjellies/systemd/system/rpistats.timer" "/etc/systemd/system/rpistats.timer"
+sudo ln -s "$HOME/dotjellies/systemd/system/plotgen.service" "/etc/systemd/system/plotgen.service"
+sudo ln -s "$HOME/dotjellies/systemd/system/plotgen.timer" "/etc/systemd/system/plotgen.timer"
 
 sudo systemctl enable certbot.timer
 sudo systemctl enable rpistats.timer
 sudo systemctl enable jstat.service
+sudo systemctl enable plotgen.timer
